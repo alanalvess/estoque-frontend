541b4336c1057ed6284c9bd7b771bab50cfb3514
Alan Alves <alan.alvess@outlook.com>
2025-05-12 20:58:55 -0300

    Melhorias de código páginas de erro

--- src/pages/erros/Erro500.tsx
+++ src/pages/erros/Erro500.tsx
@@ -11,12 +11,12 @@ export default function Erro500() {
 
     return (
         <div className='flex flex-col gap-4 items-center justify-center h-screen text-center p-4'>
-            <h1 className='text-6xl font-bold text-red-600'>500</h1>
-            <p className='text-2xl mt-4'>Erro interno do servidor</p>
-            <p className='text-gray-600 mt-2'>Algo deu errado. Tente novamente mais tarde.</p>
+            <h1 className='text-6xl font-bold text-rose-600'>500</h1>
+            <p className='text-2xl text-gray-600 dark:text-gray-300 mt-4'>Erro interno do servidor</p>
+            <p className='text-gray-600 dark:text-gray-300 mt-2 mb-10'>Algo deu errado. Tente novamente mais tarde.</p>
 
-            <Button onClick={retornar} color='alternative' className='cursor-pointer'>
-                <span className='text-xl '>Voltar para página de produtos</span>
+            <Button onClick={retornar} color='alternative' className='cursor-pointer hover:text-teal-700'>
+                <span className='text-md '>Voltar para produtos</span>
             </Button>
         </div>
     );
--- src/pages/erros/Error404.tsx
+++ src/pages/erros/Error404.tsx
@@ -1,14 +1,22 @@
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import {Button} from "flowbite-react";
 
 export default function Erro404() {
+
+    const navigate = useNavigate();
+
+    function retornar() {
+        navigate('/produtos/all');
+    }
+
     return (
         <div className='flex flex-col items-center justify-center h-screen text-center p-4'>
-            <h1 className='text-6xl font-bold text-red-600'>404</h1>
-            <p className='text-2xl mt-4'>Página não encontrada</p>
-            <p className='text-gray-600 mt-2'>A URL que você acessou não existe.</p>
-            <Link to='/' className='mt-6 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition'>
-                Voltar para a Home
-            </Link>
+            <h1 className='text-6xl font-bold text-rose-600'>404</h1>
+            <p className='text-2xl text-gray-600 dark:text-gray-300 mt-4'>Página não encontrada</p>
+            <p className='text-gray-600 dark:text-gray-300 mt-2 mb-10'>A URL que você acessou não existe.</p>
+            <Button onClick={retornar} color='alternative' className='cursor-pointer hover:text-teal-700'>
+                <span className='text-md '>Voltar para produtos</span>
+            </Button>
         </div>
     );
 }
